5ecaf9887b112e5b63f43e6b35abdd0821979182
julianluissoto <conjulianlasoto@gmail.com>
2025-07-30 22:21:32 -0300

    fix max w

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,10 +31,10 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Literata:ital,opsz,wght@0,7..72,200..900;1,7..72,200..900&display=swap" rel="stylesheet" />
       </head>
-      <body className="font-body antialiased">
+      <body className="font-body antialiased max-w-7xl mx-auto ">
         <AuthProvider>
           <FavoritesProvider>
-            <div className="flex min-h-screen bg-background">
+            <div className="flex min-h-screen bg-background justify-center">
               <Suspense fallback={<CategorySidebarSkeleton />}>
                 <CategorySidebar />
               </Suspense>
